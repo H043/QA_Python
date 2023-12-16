822cc2e157e2088d02416cc51692ee248c132f21
H043 <74686459+H043@users.noreply.github.com>
2023-12-16 12:38:27 +0530

    Update HWpython/HillelHW/hw2.py
    Co-authored-by: Settt <sergei.sydorenko@gmail.com>

--- HWpython/HillelHW/hw2.py
+++ HWpython/HillelHW/hw2.py
@@ -12,7 +12,16 @@ if user_selection == random_item:
     print("Выбор компьютера", random_item)
     print("Ничья")
 
-elif user_selection == "Камень" and random_item == "Ножницы":
+elif (
+    (user_selection == "Камень" and random_item == "Ножницы")
+    or (user_selection == "Ножницы" and random_item == "Бумага")
+    or (user_selection == "Бумага" and random_item == "Камень")
+):
+    print("Выбор компьютера", random_item)
+    print("Вы победили")
+else:
+    print("Выбор компьютера", random_item)
+    print("Вы проиграли")
     print("Выбор компьютера", random_item)
     print("Вы победили")
 elif user_selection == "Камень" and random_item == "Бумага":

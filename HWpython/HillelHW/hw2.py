b7dbc29b15556244c19e31a0ff90b19bfa9703f5
H043 <Andriy.regi@gmail.com>
2023-12-16 12:42:05 +0530

    optimized elif

--- HWpython/HillelHW/hw2.py
+++ HWpython/HillelHW/hw2.py
@@ -11,22 +11,32 @@ random_item = random.choice(choice)
 if user_selection == random_item:
     print("Выбор компьютера", random_item)
     print("Ничья")
+elif (
+    (user_selection == "Камень" and random_item == "Ножницы")
+    or (user_selection == "Ножницы" and random_item == "Бумага")
+    or (user_selection == "Бумага" and random_item == "Камень")
+):
+    print("Выбор компьютера", random_item)
+    print("Вы победили")
+else:
+    print("Выбор компьютера", random_item)
+    print("Вы проиграли")
 
-elif user_selection == "Камень" and random_item == "Ножницы":
-    print("Выбор компьютера", random_item)
-    print("Вы победили")
-elif user_selection == "Камень" and random_item == "Бумага":
-    print("Выбор компьютера", random_item)
-    print("Вы проиграли")
-elif user_selection == "Ножницы" and random_item == "Бумага":
-    print("Выбор компьютера", random_item)
-    print("Вы победили!")
-elif user_selection == "Ножницы" and random_item == "Камень":
-    print("Выбор компьютера",random_item)
-    print("Вы проиграли")
-elif user_selection == "Бумага" and random_item == "Камень":
-    print("Выбор компьютера", random_item)
-    print("Вы победили")
-elif user_selection == "Бумага" and random_item == "Ножницы":
-    print("Выбор компьютера", random_item)
-    print("Вы проиграли")
+# elif user_selection == "Камень" and random_item == "Ножницы":
+#     print("Выбор компьютера", random_item)
+#     print("Вы победили")
+# elif user_selection == "Камень" and random_item == "Бумага":
+#     print("Выбор компьютера", random_item)
+#     print("Вы проиграли")
+# elif user_selection == "Ножницы" and random_item == "Бумага":
+#     print("Выбор компьютера", random_item)
+#     print("Вы победили!")
+# elif user_selection == "Ножницы" and random_item == "Камень":
+#     print("Выбор компьютера",random_item)
+#     print("Вы проиграли")
+# elif user_selection == "Бумага" and random_item == "Камень":
+#     print("Выбор компьютера", random_item)
+#     print("Вы победили")
+# elif user_selection == "Бумага" and random_item == "Ножницы":
+#     print("Выбор компьютера", random_item)
+#     print("Вы проиграли")
